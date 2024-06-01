43972a707158bf83f69911a3fcb048e6c2a34f03
hikaru513 <138415205+hikaru513@users.noreply.github.com>
2024-06-01 22:57:37 +0900

    Add files via upload

---
+++ db.js
@@ -0,0 +1,9 @@
+module.exports = {
+    client: "mysql2",
+    connection: {
+        host: "localhost",
+        user: "root",
+        password: "1593578hH!",
+        database: "todolist"
+    }
+}
